3f345d8c6cbdc9d6cddcd5c727db07b25426da53
Ronith PR <ronithgsk@gmail.com>
2024-09-08 19:07:52 +0530

    Added custom modal

---
+++ src/components/CustomModal.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import ReactDom from "react-dom";
+
+const CustomModal = ({
+    visible,
+    setVisible,
+    children,
+    blockClosing = false,
+}: {
+    visible: boolean;
+    setVisible: (value: boolean) => void;
+    children: React.ReactNode;
+    blockClosing?: boolean;
+}) => {
+    const [domReady, setDomReady] = useState(false);
+
+    useEffect(() => {
+        setDomReady(true);
+    }, []);
+
+    useEffect(() => {
+        if (!visible) return;
+        const keyDown = (event: KeyboardEvent) => {
+            if (event.key === "Escape" && !blockClosing) {
+                setVisible(false);
+            }
+        };
+        document.addEventListener("keydown", keyDown);
+        return () => {
+            document.removeEventListener("keydown", keyDown);
+        };
+    }, [visible, blockClosing]);
+
+    useEffect(() => {
+        if (visible) {
+            document.body.style.position = "fixed";
+            document.body.style.top = "0px";
+            document.body.style.width = "100%";
+        } else {
+            const scrollY = document.body.style.top;
+            document.body.style.position = "";
+            document.body.style.top = "";
+            window.scrollTo(0, parseInt(scrollY || "0") * -1);
+        }
+
+        return () => {
+            document.body.style.position = "";
+            document.body.style.top = "";
+            document.body.style.width = "";
+        };
+    }, [visible]);
+
+    if (!visible) return null;
+
+    return domReady
+        ? ReactDom.createPortal(
+            <>
+                <div
+                    className="fixed w-screen h-screen bg-black opacity-30 top-0  left-0 flex justify-center items-center z-[999]"
+                    onClick={() => {
+                        if (blockClosing) return;
+                        setVisible(false);
+                    }}
+                />
+                <div
+                    className='fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 bg-white p-[50px] z-[1000]'
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <div
+                        className='flex items-center lg:min-w-[500px] min-h-[200px] lg:max-w-[70vw] max-h-[70vh] bg-[var(--modal-background-color)] rounded-xl py-5 px-6'
+                    >
+                        {children}
+                    </div>
+                </div>
+            </>,
+            document.getElementById("portal")!
+        )
+        : null;
+};
+
+export default CustomModal;
--- src/view/ContactsManager/ContactsManager.tsx
+++ src/view/ContactsManager/ContactsManager.tsx
@@ -1,19 +1,30 @@
+import { useState } from 'react'
 import PrimaryButton from '../../components/PrimaryButton'
+import CustomModal from '../../components/CustomModal';
 
 type Props = {}
 
 function ContactsManager({ }: Props) {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleModelClose = () => {
+    setShowModal(!showModal)
+  }
   return (
     <div className='px-5 pt-5'>
       <div className='flex justify-end mb-5'>
-      <PrimaryButton label='Create contact' onClick={() => {
-        console.log('first')
-      }} />
+      <PrimaryButton label='Create contact' onClick={handleModelClose} />
       </div>
       <div className='h-full w-full flex flex-col p-20 items-center m-auto text-center gap-2'>
         <h1 className='font-bold text-xl'>No contact found</h1>
         <h2 className='font-medium'>Please add contact from Create contact button </h2>
       </div>
+
+      <CustomModal visible={showModal} setVisible={handleModelClose}>
+        <div className='bg-white'>
+          <h1>hi</h1>
+       </div>
+       </CustomModal>
     </div>
 
   )
